64c31cb5b7a71cfb9aa30cec72e54289d25d479d
Ali Raheem <ali.raheem@gmail.com>
2018-01-03 11:18:21 +0000

    Cleaned up code

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ Really basic example
 JQuery to access electrum-vtc RPC
 
 ![screenshot](screenie.png)
+
+Start like so
+
+Make sure to backup your wallets from ~/.electrum-vtc
+
+```
+ELECTRUMCMD=electrum-vtc
+mkdir ./wallet
+$ELECTRUMCMD daemon
+$ELECTRUMCMD create
+$ELECTRUMCMD load_wallet
+$ELECTRUMCMD setconfig requests_dir ./wallet/req
+cd ./wallet
+python -m SimpleHTTPServer
+```
---
+++ wallet/wallet.js
@@ -0,0 +1,56 @@
+electrumRPC = "http://localhost:7777";
+function jsonRPC(id, method, params={}, callback = null, dataType = "json") {
+	var j = {"id":id,"method":method,"params":params};
+	request = JSON.stringify(j);
+	$.post(electrumRPC, request, callback, dataType);
+	console.log(request);
+}
+function updateBalance () {
+	console.log('getbalance');
+	jsonRPC("updateBalance","getbalance", {},
+		function(data){
+			$("#balance").html(data.result.confirmed+ " VTC");
+	});
+}
+function updateRequests () {
+	console.log('listrequests');
+	jsonRPC("updateRequests", "listrequests", {},
+	function(data, status){
+		var i;
+		var table = "<table><tr><td>Address</td><td>Amount (VTC)</td><td>Memo</td>";
+		for(i = 0; i < data.result.length; i++) {
+                	table += "<tr><td>"+data.result[i].address+"</td><td>"+ data.result[i].amount/100000000+ "</td><td>"+data.result[i].memo+"</td></tr>";
+                 }
+		table += "</table>";
+		$("#requests-table").html(table);
+        });
+}
+                 
+$(document).ready(function() {
+	$("#add-request-button").click(function() {
+		console.log('addreq');
+		var amount = $("#add-request-amount").val();
+		var memo = $("#add-request-memo").val();
+		var params = {"amount": amount, "memo": memo};
+		jsonRPC("addreq", "addrequest", params);
+	})
+;});
+$(document).ready(function() {
+	$("#clear-requests-button").click(function() {
+		console.log('clearrequests');
+		jsonRPC("clearreq", "clearrequests");
+	})
+});
+$(document).ready(function() {
+	$("#update-button").click(function() {
+		console.log('update');
+		mainloop();
+	})
+});
+function mainloop(){
+  updateBalance();
+  updateRequests();
+}
+$(document).ready(function() {
+	window.setInterval(mainloop(), 1000);
+});
